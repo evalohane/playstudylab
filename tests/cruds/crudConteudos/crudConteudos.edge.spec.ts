f9c3540777c482791ed42902f549317a868832ff
evalohane <evalohane10@gmail.com>
2026-05-31 21:26:55 -0300

    feat: adiciona teste de borda do crud de conteudos

---
+++ tests/cruds/crudConteudos/crudConteudos.edge.spec.ts
@@ -0,0 +1,18 @@
+import { test } from '@playwright/test';
+import { LoginPage } from '../../../pages/LoginPage';
+import { ConteudosPage } from '../../../pages/ConteudosPage';
+
+test.describe('CRUD Conteúdos - Borda', () => {
+    test('deve exibir erro ao tentar cadastrar conteudo com caracteres especiais no nome', async ({ page }) => {
+        const loginPage = new LoginPage(page);
+        const conteudosPage = new ConteudosPage(page);
+
+        await loginPage.goto();
+        await loginPage.login('evaomnibus@oisaulo.com', 'Lielsonli123!');
+        await loginPage.verificarLogin();
+
+        await conteudosPage.goto();
+        await conteudosPage.cadastrarConteudo('$#@%!0225', '140', 'Prof. Nayane Queiroz', '9');
+        await conteudosPage.verificarErroCampo('O nome do conteúdo não pode conter números ou caracteres especiais.');
+    });
+});
