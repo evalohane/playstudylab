ebf1e99817cf1cccbc4119b091fd8aa8e19e4099
evacstz <eva.cordeiro@aluno.ce.gov.br>
2026-06-10 17:48:59 -0300

    feat: substituição de caso triste do crud de materias

--- tests/cruds/crudMaterias/crudMaterias.sad.spec.ts
+++ tests/cruds/crudMaterias/crudMaterias.sad.spec.ts
@@ -3,19 +3,6 @@ import { LoginPage } from '../../../pages/LoginPage';
 import { MateriasPage } from '../../../pages/MateriasPage';
 
 test.describe('CRUD Matérias - Triste', () => {
-    test('deve exibir erro com numero de caracteres do nome do professor excedido', async ({ page }) => {
-        const loginPage = new LoginPage(page);
-        const materiasPage = new MateriasPage(page);
-
-        await loginPage.goto();
-        await loginPage.login('evaomnibus@oisaulo.com', 'Lielsonli123!');
-        await loginPage.verificarLogin();
-
-        await materiasPage.goto();
-        await materiasPage.cadastrarMateria('Inglês', 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', '3');
-        await materiasPage.verificarErroCampo('O nome não pode ter mais de 255 caracteres.');
-    });
-
     test('deve exibir erro ao tentar cadastrar professor com números/caracteres especiais', async ({ page }) => {
         const loginPage = new LoginPage(page);
         const materiasPage = new MateriasPage(page);
@@ -27,5 +14,18 @@ test.describe('CRUD Matérias - Triste', () => {
         await materiasPage.goto();
         await materiasPage.cadastrarMateria('Inglês', '12345678', '5');
         await materiasPage.verificarErroCampo('O campo professor não pode conter números ou caracteres especiais.');
+    });
+
+    test('deve exibir erro ao tentar cadastrar uma matéria sem selecionar a matéria', async ({ page }) => {
+        const loginPage = new LoginPage(page);
+        const materiasPage = new MateriasPage(page);
+
+        await loginPage.goto();
+        await loginPage.login('evaomnibus@oisaulo.com', 'Lielsonli123!');
+        await loginPage.verificarLogin();
+
+        await materiasPage.goto();
+        await materiasPage.cadastrarMateria('', 'Prof. Claúdio', '8');
+        await materiasPage.verificarErroCampo('Selecione ou informe a matéria.');
     });
 });
