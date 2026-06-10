4f464aa6096d267d9fdc1d8945749813e80714de
evacstz <eva.cordeiro@aluno.ce.gov.br>
2026-06-10 18:39:47 -0300

    feat: adiciona mais um teste triste no crud de conteudos

--- tests/cruds/crudConteudos/crudConteudos.sad.spec.ts
+++ tests/cruds/crudConteudos/crudConteudos.sad.spec.ts
@@ -15,4 +15,17 @@ test.describe('CRUD Conteúdos - Triste', () => {
         await conteudosPage.cadastrarConteudo('Fundamentos de Machine Learning', '', 'Prof. Renan Moreira', '8');
         await conteudosPage.verificarErroCampo('Selecione a matéria.');
     });
+
+    test('deve exibir erro ao tentar salvar conteudo com nome vazio', async ({ page }) => {
+        const loginPage = new LoginPage(page);
+        const conteudosPage = new ConteudosPage(page);
+
+        await loginPage.goto();
+        await loginPage.login('evaomnibus@oisaulo.com', 'Lielsonli123!');
+        await loginPage.verificarLogin();
+
+        await conteudosPage.goto();
+        await conteudosPage.editarConteudoParaSemNome('');
+        await conteudosPage.verificarErroCampo('Informe o nome do conteúdo.');
+    });
 });
